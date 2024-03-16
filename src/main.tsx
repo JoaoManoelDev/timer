13b657aa8b8fec7523aff3f0439db0c8f7b4fba1
João Manoel <joaomanoeldev@gmail.com>
2024-03-16 14:58:14 -0300

    chore: modify to double quotes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { App } from "./app"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
