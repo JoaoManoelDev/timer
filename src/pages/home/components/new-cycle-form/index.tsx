5b18ba87505b6b328db3e7510ebe11c067ecfc9a
João Manoel <joaomanoeldev@gmail.com>
2024-03-19 18:22:34 -0300

    feat: add useCyclesContext

--- src/pages/home/components/new-cycle-form/index.tsx
+++ src/pages/home/components/new-cycle-form/index.tsx
@@ -1,10 +1,10 @@
-import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
-import { useContext } from 'react'
-import { useFormContext } from 'react-hook-form'
-import { CyclesContext } from '../../../../contexts/CyclesContext'
+import { useFormContext } from "react-hook-form"
+
+import { useCyclesContext } from "../../../../contexts/cycles-context"
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles"
 
 export function NewCycleForm() {
-  const { activeCycle } = useContext(CyclesContext)
+  const { activeCycle } = useCyclesContext()
   const { register } = useFormContext()
 
   return (
@@ -15,7 +15,7 @@ export function NewCycleForm() {
         list="task-suggestions"
         placeholder="Dê um nome para o seu projeto"
         disabled={!!activeCycle}
-        {...register('task')}
+        {...register("task")}
       />
 
       <datalist id="task-suggestions">
@@ -33,7 +33,7 @@ export function NewCycleForm() {
         min={1}
         max={60}
         disabled={!!activeCycle}
-        {...register('minutesAmount', { valueAsNumber: true })}
+        {...register("minutesAmount", { valueAsNumber: true })}
       />
 
       <span>minutos.</span>
