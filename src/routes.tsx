8b5760071c527de6e7445de78f3c7b8562f8b0e2
João Manoel <joaomanoeldev@gmail.com>
2024-03-19 18:17:23 -0300

    feat: add home and history page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,24 @@
 import { createBrowserRouter } from "react-router-dom"
 
-import { AppLayout } from "./pages/_layouts/app"
+import { History } from "./pages/history"
+import { Home } from "./pages/home"
 import { NotFound } from "./pages/not-found"
+import { AppLayout } from "./pages/_layouts/app"
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
     errorElement: <NotFound />,
-  }
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/history",
+        element: <History />
+      },
+    ]
+  },
 ])
