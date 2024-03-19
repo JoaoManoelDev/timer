6176514c5239cedf0fb17353317e7d3c9c719de1
João Manoel <joaomanoeldev@gmail.com>
2024-03-19 18:25:33 -0300

    chore: modify to double quotes

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,22 @@
 export const defaultTheme = {
-  white: '#FFF',
+  name: "light",
+  white: "#000",
 
-  'gray-100': '#E1E1E6',
-  'gray-300': '#C4C4CC',
-  'gray-400': '#8D8D99',
-  'gray-500': '#7C7C8A',
-  'gray-600': '#323238',
-  'gray-700': '#29292E',
-  'gray-800': '#202024',
-  'gray-900': '#121214',
+  "gray-900": "#D1D1D6",
+  "gray-800": "#C4C4CC",
+  "gray-700": "#8D8D99",
+  "gray-600": "#7C7C8A",
+  "gray-500": "#323238",
+  "gray-400": "#29292E",
+  "gray-300": "#202024",
+  "gray-100": "#121214",
 
-  'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
+  "green-300": "#00B37E",
+  "green-500": "#00875F",
+  "green-700": "#015F43",
 
-  'red-500': '#AB222E',
-  'red-700': '#7A1921',
+  "red-500": "#AB222E",
+  "red-700": "#7A1921",
 
-  'yellow-500': '#FBA94C',
+  "yellow-500": "#FBA94C",
 }
