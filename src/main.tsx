507cda69558abec0259e20acf9abd8fd516ba98c
João Manoel <joaomanoeldev@gmail.com>
2024-03-19 18:26:34 -0300

    feat: add CyclesContextProvider and ToggleThemeContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 import { App } from "./app"
+import { ToggleThemeContextProvider } from "./contexts/toggle-theme-context"
+import { CyclesContextProvider } from "./contexts/cycles-context"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <CyclesContextProvider>
+      <ToggleThemeContextProvider>
+        <App />
+      </ToggleThemeContextProvider>
+    </CyclesContextProvider>
   </React.StrictMode>,
 )
