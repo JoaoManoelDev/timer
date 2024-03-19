6693ec9a4a14a7cef3752c75f49eeaf655a3ac30
João Manoel <joaomanoeldev@gmail.com>
2024-03-19 18:14:58 -0300

    chore: modify to double quotes

--- src/reducers/cycles/actions.ts
+++ src/reducers/cycles/actions.ts
@@ -1,9 +1,9 @@
-import { Cycle } from './reducer'
+import { Cycle } from "./reducer"
 
 export enum ActionTypes {
-  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
-  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
-  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
+  ADD_NEW_CYCLE = "ADD_NEW_CYCLE",
+  INTERRUPT_CURRENT_CYCLE = "INTERRUPT_CURRENT_CYCLE",
+  MARK_CURRENT_CYCLE_AS_FINISHED = "MARK_CURRENT_CYCLE_AS_FINISHED",
 }
 
 export function addNewCycleAction(newCycle: Cycle) {
