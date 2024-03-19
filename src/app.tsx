12f7df1a6a205ac4cfa0d94a6f3c615b74a12450
João Manoel <joaomanoeldev@gmail.com>
2024-03-19 18:26:13 -0300

    feat: add dynamic theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,19 @@
-import { ThemeProvider } from 'styled-components'
-import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from "styled-components"
+import { RouterProvider } from "react-router-dom"
 
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
-import { routes } from './routes'
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+import { dark } from "./styles/themes/dark"
+import { useToggleThemeContext } from "./contexts/toggle-theme-context"
+import { routes } from "./routes"
 
 export const App = () => {
+  const { activeTheme } = useToggleThemeContext()
+
+  const theme = activeTheme === "light" ? defaultTheme : dark
+
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={theme}>
       <RouterProvider router={routes} />
       <GlobalStyle />
     </ThemeProvider>
