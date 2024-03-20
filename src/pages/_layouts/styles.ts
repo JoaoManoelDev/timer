269d659dfd36933d395bd145c4c6dde850835e5d
João Manoel <joaomanoeldev@gmail.com>
2024-03-20 12:28:46 -0300

    styles: add toggle in correct position

--- src/pages/_layouts/styles.ts
+++ src/pages/_layouts/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
 export const LayoutContainer = styled.div`
   max-width: 74rem;
@@ -6,15 +6,19 @@ export const LayoutContainer = styled.div`
   margin: 5rem auto;
   padding: 2.5rem;
 
-  background: ${(props) => props.theme['gray-800']};
+  background: ${(props) => props.theme["gray-800"]};
   border-radius: 8px;
 
   display: flex;
   flex-direction: column;
+  position: relative;
 `
 
 export const Toggle = styled.div`
   margin-top: auto;
   display: flex;
   justify-content: end;
+  position: absolute;
+  bottom: 1.5rem;
+  right: 1.5rem;
 `
